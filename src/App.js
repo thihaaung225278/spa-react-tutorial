fb42a6fa3750adf5216a9d2635445da479d8c205
Thiha Aung <thiha@Thihas-MacBook-Pro.local>
2023-04-17 21:57:26 +0630

    two ways of using image in react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import Image from './assets/reactjs.jpeg';
+
 import './App.css';
 
 function App() {
@@ -6,7 +8,18 @@ function App() {
   let name = "Thiha Aung"
 
   return (
-    <h1>Hello {name}</h1>
+    <div>
+      <h1>Hello {name}</h1>
+      {/* 1st way (public folder way) */}
+      {/* absolute path */}
+      {/* <img src="http://localhost:3000/reactjs.jpg" alt="image" /> */}
+      {/* relative path */}
+      {/* <img src="/reactjs.jpg" alt="image" /> */}
+
+      {/* 2nd way - import way (many people using this way) */}
+      <img src={Image} alt="reactjs" />
+    </div>
+    
   );
 }
 

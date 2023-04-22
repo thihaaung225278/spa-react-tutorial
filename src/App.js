0d2afabc4e95abba68c1f98a8f0c15ccc06d1641
Thiha Aung <thiha@Thihas-MacBook-Pro.local>
2023-04-22 16:31:58 +0630

    delete post example

--- src/App.js
+++ src/App.js
@@ -1,32 +1,56 @@
 import React from 'react'
+import './App.css'
 import { useState } from 'react'
 
 export default function App() {
 
-  let [count, setCount] = useState(0)
+  // let name = "Thiha Aung";
+  let [name, setName] = useState("Thiha Aung")
 
-  // we should not use to original state data (count)
-  let incrementCount = () =>{
+  let [posts, setPosts] = useState([
+    {
+      'id': 1,
+      'title': 'Post One'
+    },
+    {
+      'id': 2,
+      'title': 'Post Two'
+    },
+    {
+      'id': 3,
+      'title': 'Post Three'
+    },
+    {
+      'id': 4,
+      'title': 'Post Four'
+    },
+    {
+      'id': 5,
+      'title': 'Post Five'
+    }
+  ]);
 
-    // we think that is increase 4 the following code , but it not increase 4 , just only increase 1 because we use original state(count)
+  let changeName = () =>{
+    setName('Maung Maung')
+  }
 
-    // Should not write this way
-    // setCount(count+1)
-    // setCount(count+1)
-    // setCount(count+1)
-    // setCount(count+1)
-
-    // should write this way
-    setCount((prevStateCount) => prevStateCount + 1 )
-    setCount((prevStateCount) => prevStateCount + 1 )
-    setCount((prevStateCount) => prevStateCount + 1 )
+  // how to use prevState (we should not change to the original posts)
+  // should we use to prevState
+  let deletePost = (id) =>{
+    setPosts((prevState) => prevState.filter( post => post.id != id ))
   }
 
   return (
-    <div>
-      <h1>Counter</h1>
-      <h3>Count  - {count}</h3>
-      <button onClick={incrementCount}>increment</button>
+    <div className="app">
+      <h1>My name is <span>{name}</span></h1>
+      <button onClick={changeName}>change name</button>
+
+      <h1>Posts</h1>
+      {/* we need to add param at onClick function , should we use function reference pass way */}
+      <ul className="post-list">
+        {posts.map( post => <li key={post.id}>{post.title} <button onClick={()=>deletePost(post.id)}>Delete</button></li> )}
+      </ul>
+
     </div>
   )
 }

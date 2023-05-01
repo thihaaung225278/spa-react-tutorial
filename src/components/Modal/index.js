8314042146a39138d094b51d083799a0c0e9c4f2
Thiha Aung <thiha@Thihas-MacBook-Pro.local>
2023-05-01 23:44:16 +0630

    create modal box portal outer the root container

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './index.css'
+import ReactDOM from 'react-dom'
 
 export default function Modal({children}) {
   return (
-    <div className="modal-backdoor">
-      <div className="modal">
-        {children}
-      </div>
-    </div>
+    ReactDOM.createPortal(
+      <div className="modal-backdoor">
+        <div className="modal">
+          {children}
+        </div>
+      </div>, document.getElementById('modal-box')
+    )
   )
 }

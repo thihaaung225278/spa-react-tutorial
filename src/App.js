6a2cb8ed28c545bdcaea21394fe0ee11923fafca
Thiha Aung <thiha@Thihas-MacBook-Pro.local>
2023-05-01 17:31:02 +0630

    conditional rendering and change Boolean with 'git add .' (trick)

--- src/App.js
+++ src/App.js
@@ -1,56 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
-import { useState } from 'react'
 
 export default function App() {
 
-  // let name = "Thiha Aung";
-  let [name, setName] = useState("Thiha Aung")
+  let [name, setName] = useState('Aung Aung')
 
   let [posts, setPosts] = useState([
     {
-      'id': 1,
-      'title': 'Post One'
+      'id'      : 1,
+      'title' : 'Post One'
     },
     {
-      'id': 2,
-      'title': 'Post Two'
+      'id'      : 2,
+      'title' : 'Post Two'
     },
     {
-      'id': 3,
-      'title': 'Post Three'
+      'id'      : 3,
+      'title' : 'Post Three'
     },
     {
-      'id': 4,
-      'title': 'Post Four'
+      'id'      : 4,
+      'title' : 'Post Four'
     },
-    {
-      'id': 5,
-      'title': 'Post Five'
-    }
-  ]);
+  ])
 
-  let changeName = () =>{
-    setName('Maung Maung')
+  let changeName = function(){
+    setName("Maung Maung")
   }
 
-  // how to use prevState (we should not change to the original posts)
-  // should we use to prevState
-  let deletePost = (id) =>{
-    setPosts((prevState) => prevState.filter( post => post.id != id ))
+  let deletePost = function(id){
+    setPosts((prevState) => prevState.filter( post => post.id !== id ))
   }
 
   return (
-    <div className="app">
-      <h1>My name is <span>{name}</span></h1>
-      <button onClick={changeName}>change name</button>
+    <div>
+      <h1>Hi! {name}</h1>
+      <button onClick={changeName}>Change Name...</button>
 
-      <h1>Posts</h1>
-      {/* we need to add param at onClick function , should we use function reference pass way */}
-      <ul className="post-list">
-        {posts.map( post => <li key={post.id}>{post.title} <button onClick={()=>deletePost(post.id)}>Delete</button></li> )}
+      <h2>List Items</h2>
+      <ul>  
+        {/* !! နဲ့ Boolean ပြောင်းပြသွား / condition စစ်ပြသွား */}
+        {
+            !!posts.length && posts.map(post => (
+            <li key={post.id}>{post.title} <button onClick={()=>deletePost(post.id)}>Delete Post</button></li>
+        ))
+        }
+        {
+          !posts.length && <h3>There have no posts....</h3>
+        }
       </ul>
-
     </div>
   )
 }
